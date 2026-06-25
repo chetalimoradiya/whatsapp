1f6286045cae9e175bb0a007769388ecc09efd29
chetalimoradiya <chetalimoradiya@gmail.comgit --listgit config --global user.email chetalimoradiya@gmail.com>
2026-06-25 16:29:51 +0530

    chat app

--- lib/socket.ts
+++ lib/socket.ts
@@ -11,22 +11,22 @@
 //---------------deploy------------------
 
 
-// import { io } from "socket.io-client";
-
-// export const socket = io(
-//   "https://whatsapp-b0kiesa2e-chetali.vercel.app/",
-//   {
-//     autoConnect: false
-//   }
-// );
-
-
-//-----ipaddress-----------
 import { io } from "socket.io-client";
 
 export const socket = io(
-  "http://192.168.1.102:3001",
+  "https://whatsapp-b0kiesa2e-chetali.vercel.app/",
   {
     autoConnect: false
   }
 );
+
+
+//-----ipaddress-----------
+// import { io } from "socket.io-client";
+
+// export const socket = io(
+//   "http://192.168.1.102:3001",
+//   {
+//     autoConnect: false
+//   }
+// );
