26a974f14377162569775136e5df1c35298d0056
chetalimoradiya <chetalimoradiya@gmail.comgit --listgit config --global user.email chetalimoradiya@gmail.com>
2026-06-25 15:15:26 +0530

    socket url update

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,7 +1,17 @@
+// import { io } from "socket.io-client";
+
+// export const socket = io(
+//   "http://localhost:3001",
+//   {
+//     autoConnect: false
+//   }
+// );
+
+
 import { io } from "socket.io-client";
 
 export const socket = io(
-  "http://localhost:3001",
+  "https://whatsapp-b0kiesa2e-chetali.vercel.app/",
   {
     autoConnect: false
   }
